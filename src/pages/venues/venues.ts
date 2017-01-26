d2705756855a4cd469b5aac64f0cf3aa20381fd5
ljopleda <ljopleda@gmail.com>
2017-01-26 11:30:20 +0800

    Single venue added

--- src/pages/venues/venues.ts
+++ src/pages/venues/venues.ts
@@ -13,40 +13,65 @@ import { LocalStorageService } from 'angular-2-local-storage';
 export class VenuesPage {
 
 	public venue_cities = [];
+	public venues = [];
 	public link = 'http://www.gigsmanila.com/api/venues/';
 	public cur_page = 1;
 	public cur_limit = 10;
+	public show_city: boolean = false;
+	public show_venues: boolean = false;
+	public show_venue: boolean = false;
+	public city: string = '';
+	public venue: string = '';
 
 	constructor(public navCtrl: NavController,  public loadingCtrl:LoadingController, public alertCtrl: AlertController, public http: Http, public appCtrl: App, public navParams: NavParams) {
-		let city_ID = navParams.get('cityID');
+		let cityID = navParams.get('cityID');
+		let cityName = navParams.get('cityName');
+		let venueDetails = navParams.get('venueDetails');
 
 		let loader = this.loadingCtrl.create({
 		    content: "Please wait..."
 		});
 		loader.present();
 
-		if(typeof city_ID == 'undefined'){
+		if(typeof venueDetails !== 'undefined'){
+			this.venue = venueDetails.name;
+			console.log(venueDetails);
+			this.show_city = false;
+			this.show_venues = false;
+			this.show_venue = true;
+			loader.dismissAll();
+		}else if(typeof cityID == 'undefined'){
 			let response$ = this.http.get(this.link+this.cur_page+'/'+this.cur_limit+'/').map((res) => res.json());
 			response$.subscribe(
 				response => {
 					this.venue_cities = response;
 					console.log(response);
+					this.show_city = true;
+					this.show_venues = false;
+					this.show_venue = false;
 					loader.dismissAll();
 				}, error => {
 					loader.dismissAll();
 					let alert = this.alertCtrl.create({
 						title:'', 
-						subTitle:'Fetching venues failed!',
+						subTitle:'Fetching cities failed!',
 						buttons:['OK']
 					});
 					alert.present();
 				}
 			);
 		}else{
-			let response$ = this.http.get(this.link+city_ID+'/').map((res) => res.json());
+			if(typeof cityName !== 'undefined')
+				this.city = 'Venues In '+cityName;
+			else
+				this.city = 'Venues';
+			let response$ = this.http.get(this.link+cityID+'/').map((res) => res.json());
 			response$.subscribe(
 				response => {
-					this.venue_cities = response;
+					this.venues = response;
+					this.show_city = false;
+					this.show_venues = true;
+					this.show_venue = false;
 					console.log(response);
 					loader.dismissAll();
 				}, error => {
@@ -62,9 +87,15 @@ export class VenuesPage {
 		}
 	}
 
-	getVenues(cityID){
+	getVenueDetails(venueDetails){
 		this.appCtrl.getRootNav().push(VenuesPage,{
-	  		cityID: cityID
+	  		venueDetails: venueDetails
+	  	});
+	}
+	getVenues(cityID,cityName){
+		this.appCtrl.getRootNav().push(VenuesPage,{
+	  		cityID: cityID,
+	  		cityName: cityName
 	  	});
 	}
 }
